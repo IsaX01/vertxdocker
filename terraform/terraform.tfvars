bf0e56f9b1b28ade047750e48e27fb1af1135efb
IsaX01 <isaiaybadia@gmail.com>
2024-10-27 20:15:24 -0400

    Add: terraform files

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+project  = "xeventsnotifications"
+region      = "us-central1"
+zone        = "us-central1-c"
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "project" {
+  description = "xeventsnotifications"
+}
+
+variable "region" {
+  description = "us-central1"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "us-central1-c"
+  default     = "us-central1-c"
+}
